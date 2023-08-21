d9f64abded9041df8f4d039baeb9d9dc4a0fbeec
Afolabi Abdulqadri <afolabizerotohero@gmail.com>
2023-08-21 23:28:23 +0100

    Update extract.py

--- dev/extract.py
+++ dev/extract.py
@@ -7,11 +7,8 @@ response = s3.list_objects(Bucket=bucket_name, Prefix="orders_data")
 print(response)
 # download the orders.csv
 s3.download_file(bucket_name, "orders_data/orders.csv", "orders.csv")
-# download the orders.csv
+# download the reviews.csv
 s3.download_file(bucket_name, "orders_data/reviews.csv", "reviews.csv")
-# download the orders.csv
+# download the shipment_deliveries.csv
 s3.download_file(bucket_name, "orders_data/shipment_deliveries.csv", "shipment_deliveries.csv")
-# download the orders.csv
-s3.download_file(bucket_name, "analytics_export/elvidavi3768/best_performing_product.csv", "aaaa2.csv")
-
 
