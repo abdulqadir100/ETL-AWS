9e44a3f0853eaef6d9b064e5011813b7d08b3fb3
Afolabi Abdulqadri <afolabizerotohero@gmail.com>
2023-09-07 23:53:16 +0100

    Update extract.py

--- dev/extract.py
+++ dev/extract.py
@@ -2,7 +2,7 @@ import boto3
 from botocore import UNSIGNED
 from botocore.client import Config
 s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
-bucket_name = "d2b-internal-assessment-bucket"
+bucket_name = "insert-bucket-name"
 response = s3.list_objects(Bucket=bucket_name, Prefix="orders_data")
 print(response)
 # download the orders.csv
